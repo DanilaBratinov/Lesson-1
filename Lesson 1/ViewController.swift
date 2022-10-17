a17978f9c14cc5d40f32af0da78aa1c605734479
DanilaBratinov <bratinov0704@inbox.ru>
2022-10-17 21:42:07 +0300

    code

--- Lesson 1/ViewController.swift
+++ Lesson 1/ViewController.swift
@@ -1,17 +1,17 @@
-//
-//  ViewController.swift
-//  Lesson 1
-//
-//  Created by Данила Братинов on 17.10.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // code
+        /*
+         code
+         code
+         code
+         code
+         code...
+         */
     }
 
 
