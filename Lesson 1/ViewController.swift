303ce0a57210418be77ff89cb3b128d975e5e4fe
DanilaBratinov <bratinov0704@inbox.ru>
2022-10-17 21:55:02 +0300

    Commit

--- Lesson 1/ViewController.swift
+++ Lesson 1/ViewController.swift
@@ -4,14 +4,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // code
-        /*
-         code
-         code
-         code
-         code
-         code...
-         */
+
     }
 
 
